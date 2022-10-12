a8871b7f9857cd356678f948b12acd49e71829fe
suzukey <suzukei0208@gmail.com>
2022-10-12 20:47:55 +0900

    🎨 タグ変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,10 @@
     class="py-40 text-center bg-gradient-to-br from-light-blue-300 to-blue-400 text-white text-shadow"
   >
     <h2 class="text-3xl font-bold">楽をつくる、楽しいをつくる</h2>
-    <h3 class="mt-2 text-xl">Creating Ease, Creating Fun</h3>
+    <p class="mt-2 text-xl">Creating Ease, Creating Fun</p>
   </div>
   <div id="products" class="px-20 py-10 bg-white">
-    <h4 class="text-2xl font-bold">Products</h4>
+    <h3 class="text-2xl font-bold">Products</h3>
     <div class="pt-4">
       <p v-for="i in 20" :key="i">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
@@ -17,7 +17,7 @@
     </div>
   </div>
   <div id="about" class="px-20 py-10 bg-dark-100 text-white">
-    <h4 class="text-2xl font-bold">About</h4>
+    <h3 class="text-2xl font-bold">About</h3>
     <div class="pt-4">
       <p v-for="i in 20" :key="i">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
@@ -28,7 +28,7 @@
     </div>
   </div>
   <div id="contact" class="px-20 py-10 bg-white">
-    <h4 class="text-2xl font-bold">Contact</h4>
+    <h3 class="text-2xl font-bold">Contact</h3>
     <div class="pt-4">
       <p v-for="i in 20" :key="i">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
