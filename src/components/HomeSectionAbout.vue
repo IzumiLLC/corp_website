2f59fbacf44f97bef633b23cfe2307d0a9361bd7
suzukey <suzukei0208@gmail.com>
2022-11-13 22:34:47 +0900

    ✨ Aboutセクション作成

---
+++ src/components/HomeSectionAbout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { Corp } from '~/types'
+
+defineProps<{
+  corp: Corp | null
+}>()
+</script>
+
+<template>
+  <div id="about" class="py-10 bg-dark-100 text-white">
+    <InnerWrap>
+      <h3 class="text-2xl font-bold">About</h3>
+      <div class="pt-4">
+        <dl>
+          <dt class="text-lg font-bold">会社名</dt>
+          <dd>{{ corp?.name }}</dd>
+          <dt class="text-lg font-bold mt-2">本店所在地</dt>
+          <dd>{{ corp?.location }}</dd>
+          <dt class="text-lg font-bold mt-2">代表社員</dt>
+          <dd>{{ corp?.representative }}</dd>
+          <dt class="text-lg font-bold mt-2">設立</dt>
+          <dd>{{ corp?.established }}</dd>
+          <dt class="text-lg font-bold mt-2">社員数</dt>
+          <dd>{{ `${corp?.employees}名` }}</dd>
+        </dl>
+        <p class="mt-2 text-xs">{{ `${corp?.updatedAt} 時点` }}</p>
+      </div>
+    </InnerWrap>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,16 @@
 <script setup lang="ts">
 import { useFetch } from '#imports'
 
-const [{ data: products }] = await Promise.all([
+const [{ data: products }, { data: corp }] = await Promise.all([
   await useFetch('/api/products'),
+  await useFetch('/api/corp'),
 ])
 </script>
 
 <template>
   <HomeSectionHero />
   <HomeSectionProducts :products="products" />
-  <div id="about" class="px-20 py-10 bg-dark-100 text-white">
-    <h3 class="text-2xl font-bold">About</h3>
-    <div class="pt-4">
-      <p v-for="i in 20" :key="i">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
-        maxime magnam, dolorum sapiente possimus, voluptatum quo saepe aliquid
-        facere repellat aperiam ut nemo perspiciatis veniam facilis corporis
-        neque quae error!
-      </p>
-    </div>
-  </div>
+  <HomeSectionAbout :corp="corp" />
   <div id="contact" class="px-20 py-10 bg-white">
     <h3 class="text-2xl font-bold">Contact</h3>
     <div class="pt-4">
